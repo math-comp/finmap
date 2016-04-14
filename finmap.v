9f840086553d4a17facac478d703f3d33d6c5f38
Pierre-Yves Strub <pierre-yves@strub.nu>
2016-04-14 16:44:34 +0200

    Add misc. big/fset lemmas.

--- finmap.v
+++ finmap.v
@@ -2278,3 +2278,15 @@ by have [->|//] := altP eqP; rewrite fnd_rem in_fsetE eqxx.
 Qed.
 
 End FinMapKeyType.
+
+Section BigFSet.
+Variable (R : Type) (idx : R) (op : Monoid.law idx).
+
+Lemma big_fset0_cond (T : choiceType) (P : pred _) (F : _ -> R) :
+  \big[op/idx]_(i : @fset0 T | P i) F i = idx :> R.
+Proof. by apply: big_pred0 => -[j hj]; have := hj; rewrite in_fset0. Qed.
+
+Lemma big_fset0 (T : choiceType) (F : _ -> R) :
+  \big[op/idx]_(i : @fset0 T) F i = idx :> R.
+Proof. by apply/(big_fset0_cond xpredT). Qed.
+End BigFSet.
